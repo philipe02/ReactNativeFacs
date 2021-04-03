5f678dc6fe22b2185b2f29b790e28143c18cc004
Philipe <philipe_02@hotmail.com>
2021-04-03 12:01:38 -0300

    criando estrutura inicial

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
+import { Header } from "react-native-elements";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { styles } from "./style/style";
+import Ideias from "./components/Ideias";
+import Usuario from "./components/Usuario";
+import Feed from "./components/Feed";
+
+const Drawer = createDrawerNavigator();
+
+const TelaInicial = ({ navigation }) => {
+  return (
+    <>
+      <Header
+        containerStyle={{ height: 80 }}
+        leftComponent={{
+          icon: "menu",
+          color: "#fff",
+          onPress: navigation.openDrawer,
+          size: 40,
+        }}
+        centerComponent={{
+          text: "Tela inicial",
+          style: styles.headerText,
+        }}
+        rightComponent={{
+          icon: "home",
+          color: "#fff",
+          size: 40,
+          onPress: () => navigation.navigate("Inicio"),
+        }}
+      />
+      <View style={styles.container}>
+        <Text>Tela inicial</Text>
+      </View>
+      <StatusBar style="light" />
+    </>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Inicio">
+        <Drawer.Screen name="Inicio" component={TelaInicial} />
+        <Drawer.Screen name="Ideias" component={Ideias} />
+        <Drawer.Screen name="Usuario" component={Usuario} />
+        <Drawer.Screen name="Feed" component={Feed} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerText: {
+    color: "#fff",
+    fontSize: 20,
+    paddingTop: 7,
+  },
+});
