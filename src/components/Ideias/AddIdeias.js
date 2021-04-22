8a9425b4e68af3c6353452be143715ba76afeb3f
nathalieEvans <nathalie.evangelista@hotmail.com>
2021-04-21 23:56:41 -0300

    Alterações do ID

--- src/components/Ideias/AddIdeias.js
+++ src/components/Ideias/AddIdeias.js
@@ -1,172 +1,201 @@
 import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
-import { View, Text, ImageBackground, TouchableOpacity, Modal, Alert} from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Modal,
+  Alert,
+} from "react-native";
 import { styles } from "../../style/style";
-import { Header, Input  } from "react-native-elements";
-import {Picker} from '@react-native-picker/picker';
+import { Header, Input } from "react-native-elements";
+import { Picker } from "@react-native-picker/picker";
 
 const AddIdeias = (props) => {
-
   const initialIdeiasState = {
-    id:"",
-    titulo:"",
-    desc: "", 
-    setor: "Selecione um setor", 
-    tema: "Selecione um tema", 
-    benefMalef: ""
-  }
+    titulo: "",
+    desc: "",
+    setor: "Selecione um setor",
+    tema: "Selecione um tema",
+    benefMalef: "",
+  };
 
-  const [ideia, setIdeia] = useState(initialIdeiasState)
-  const { openIdeiaModal, closeIdeiaModal } = props
+  const [ideia, setIdeia] = useState(initialIdeiasState);
+  const { openIdeiaModal, closeIdeiaModal } = props;
 
   const handleChance = (value, name) => {
-    setIdeia({...ideia, [name]: value})
-  }
+    setIdeia({ ...ideia, [name]: value });
+  };
 
   const addIdeia = async () => {
     //validação de campos obrigatórios
-    if(!ideia.titulo || ideia.titulo === "")
-      Alert.alert("Título Obrigatório.")
-    else if(!ideia.desc || ideia.desc === "")
-      Alert.alert("Descrição Obrigatória.")
-    else if(!ideia.benefMalef || ideia.benefMalef === "")
-      Alert.alert("Descreva pelo menos um malefício/benefício.")
-    else if(ideia.tema === "Selecione um tema")
-      Alert.alert("Selecione o tema abordado.")
-    else if(ideia.setor === "Selecione um setor")
-      Alert.alert("Selecione o setor ao qual sua ideia se relaciona.")
-    else{
-      props.addIdeias(ideia)
-      props.closeIdeiaModal();       
+    if (!ideia.titulo || ideia.titulo === "")
+      Alert.alert("Título Obrigatório.");
+    else if (!ideia.desc || ideia.desc === "")
+      Alert.alert("Descrição Obrigatória.");
+    else if (!ideia.benefMalef || ideia.benefMalef === "")
+      Alert.alert("Descreva pelo menos um malefício/benefício.");
+    else if (ideia.tema === "Selecione um tema")
+      Alert.alert("Selecione o tema abordado.");
+    else if (ideia.setor === "Selecione um setor")
+      Alert.alert("Selecione o setor ao qual sua ideia se relaciona.");
+    else {
+      props.adicionarIdeia(ideia);
+      props.closeIdeiaModal();
     }
-  }
+  };
 
-    return (
-            <View style={styles.container}>
-              
-              <Modal 
-                animationType="fase" 
-                transparent={false} 
-                visible={openIdeiaModal}
-                onRequestClose={closeIdeiaModal}  
-              >
-                <Header
-                  containerStyle={{ height: 50, backgroundColor: "#1D1D1D" }}
-                  centerComponent={{
-                    text: "Suas ideias",
-                    style: styles.headerText,
-                  }}
-                />
-                <ImageBackground source={require('../../images/fundo1.png')} style={styles.bgImage}>            
-                <Text style={styles.tituloInput}>Título: </Text>
-                <Input
-                  placeholder="Como você chama essa ideia ?"
-                  inputStyle={{
-                    paddingLeft:10, 
-                    paddingTop:2,
-                    backgroundColor:'#fff',
-                    borderRadius:4,
-                    marginRight:8
-                  }}
-                  containerStyle={{
-                    justifyContent:'center',
-                  }}
-                  errorStyle={{
-                    height:0
-                  }}
-                  onChangeText={(text) => {handleChance(text, "titulo")}}
-                />
-                <Text style={styles.tituloInput}>Descrição: </Text>
-                <Input
-                  placeholder="Descreva como seria sua ideia ?"
-                  inputStyle={{
-                    paddingLeft:10, 
-                    paddingTop:2,
-                    backgroundColor:'#fff',
-                    borderRadius:4,
-                    justifyContent:'center',
-                    marginRight:8
-                  }}
-                  containerStyle={{
-                    justifyContent:'center',
-                  }}
-                  multiline={true}
-                  numberOfLines={4}
-                  maxLength={150}
-                  errorStyle={{
-                    height:0
-                  }}
-                  onChangeText={(text) => {handleChance(text, "desc")}}
-                />
+  return (
+    <View style={styles.container}>
+      <Modal
+        animationType="fase"
+        transparent={false}
+        visible={openIdeiaModal}
+        onRequestClose={closeIdeiaModal}
+      >
+        <Header
+          containerStyle={{ height: 50, backgroundColor: "#1D1D1D" }}
+          centerComponent={{
+            text: "Suas ideias",
+            style: styles.headerText,
+          }}
+        />
+        <ImageBackground
+          source={require("../../images/fundo1.png")}
+          style={styles.bgImage}
+        >
+          <Text style={styles.tituloInput}>Título: </Text>
+          <Input
+            placeholder="Como você chama essa ideia ?"
+            inputStyle={{
+              paddingLeft: 10,
+              paddingTop: 2,
+              backgroundColor: "#fff",
+              borderRadius: 4,
+              marginRight: 8,
+            }}
+            containerStyle={{
+              justifyContent: "center",
+            }}
+            errorStyle={{
+              height: 0,
+            }}
+            onChangeText={(text) => {
+              handleChance(text, "titulo");
+            }}
+          />
+          <Text style={styles.tituloInput}>Descrição: </Text>
+          <Input
+            placeholder="Descreva como seria sua ideia ?"
+            inputStyle={{
+              paddingLeft: 10,
+              paddingTop: 2,
+              backgroundColor: "#fff",
+              borderRadius: 4,
+              justifyContent: "center",
+              marginRight: 8,
+            }}
+            containerStyle={{
+              justifyContent: "center",
+            }}
+            multiline={true}
+            numberOfLines={4}
+            maxLength={150}
+            errorStyle={{
+              height: 0,
+            }}
+            onChangeText={(text) => {
+              handleChance(text, "desc");
+            }}
+          />
 
-                <Text style={styles.tituloInput}>Benefícios/Malefícios: </Text>
-                <Input
-                  placeholder="Quais seriam os benefícios/malefícos que essa ideia pode resolver/trazer?"
-                  inputStyle={{
-                    paddingLeft:10, 
-                    paddingTop:2,
-                    backgroundColor:'#fff',
-                    borderRadius:4,
-                    marginRight:8
-                  }}
-                  containerStyle={{
-                    justifyContent:'center',
-                  }}
-                  multiline={true}
-                  numberOfLines={5}
-                  maxLength={200}
-                  errorStyle={{
-                    height:0
-                  }}
-                  onChangeText={(text) => {handleChance(text, "benefMalef")}}
-                />
+          <Text style={styles.tituloInput}>Benefícios/Malefícios: </Text>
+          <Input
+            placeholder="Quais seriam os benefícios/malefícos que essa ideia pode resolver/trazer?"
+            inputStyle={{
+              paddingLeft: 10,
+              paddingTop: 2,
+              backgroundColor: "#fff",
+              borderRadius: 4,
+              marginRight: 8,
+            }}
+            containerStyle={{
+              justifyContent: "center",
+            }}
+            multiline={true}
+            numberOfLines={5}
+            maxLength={200}
+            errorStyle={{
+              height: 0,
+            }}
+            onChangeText={(text) => {
+              handleChance(text, "benefMalef");
+            }}
+          />
 
-                <Text style={styles.tituloInput}>Tema: </Text>
-                <Picker
-                  selectedValue={ideia.tema}
-                  onValueChange={(itemValue) => handleChance(itemValue, "tema")}
-                  style={{ height: 40, width: 345, marginLeft:8, backgroundColor:'#fff'}}
-                >
-                  <Picker.Item label="Selecione o tema abordado" value="temaPadrao"/>
-                  <Picker.Item label="Recursos Humanos" value="recursoshumanos"/>
-                  <Picker.Item label="TI" value="TI"/>
-                  <Picker.Item label="Administração" value="adm"/>
-                  <Picker.Item label="Finanças" value="financas"/>
-                </Picker>
+          <Text style={styles.tituloInput}>Tema: </Text>
+          <Picker
+            selectedValue={ideia.tema}
+            onValueChange={(itemValue) => handleChance(itemValue, "tema")}
+            style={{
+              height: 40,
+              width: 345,
+              marginLeft: 8,
+              backgroundColor: "#fff",
+            }}
+          >
+            <Picker.Item label="Selecione o tema abordado" value="temaPadrao" />
+            <Picker.Item label="Recursos Humanos" value="recursoshumanos" />
+            <Picker.Item label="TI" value="TI" />
+            <Picker.Item label="Administração" value="adm" />
+            <Picker.Item label="Finanças" value="financas" />
+          </Picker>
 
-                <Text style={styles.tituloInput}>Setor: </Text>
-                <Picker
-                  selectedValue={ideia.setor}
-                  onValueChange={(itemValue) => handleChance(itemValue, "setor")}
-                  style={{ height: 40, width: 345, marginLeft:8, backgroundColor:'#fff'}}
-                >
-                  <Picker.Item label="Selecione um setor" value="setorPadrao" />
-                  <Picker.Item label="Área de Pessoas" value="pessoas"/>
-                  <Picker.Item label="Tecnologia da informação" value="tinformação"/>
-                  <Picker.Item label="Gerência e gestão" value="gestao"/>
-                  <Picker.Item label="Contabilidade" value="contabilidade"/>
-                </Picker>
+          <Text style={styles.tituloInput}>Setor: </Text>
+          <Picker
+            selectedValue={ideia.setor}
+            onValueChange={(itemValue) => handleChance(itemValue, "setor")}
+            style={{
+              height: 40,
+              width: 345,
+              marginLeft: 8,
+              backgroundColor: "#fff",
+            }}
+          >
+            <Picker.Item label="Selecione um setor" value="setorPadrao" />
+            <Picker.Item label="Área de Pessoas" value="pessoas" />
+            <Picker.Item label="Tecnologia da informação" value="tinformação" />
+            <Picker.Item label="Gerência e gestão" value="gestao" />
+            <Picker.Item label="Contabilidade" value="contabilidade" />
+          </Picker>
 
-                <View style={styles.botaoContainer}>
-                  <TouchableOpacity
-                    onPress={addIdeia}
-                    style={{...styles.botaoSalveCancel, backgroundColor:"#1281AB",}}
-                  >
-                    <Text style={styles.textBotaoSalveCancel}>Salvar</Text>
-                  </TouchableOpacity>
+          <View style={styles.botaoContainer}>
+            <TouchableOpacity
+              onPress={addIdeia}
+              style={{ ...styles.botaoSalveCancel, backgroundColor: "#1281AB" }}
+            >
+              <Text style={styles.textBotaoSalveCancel}>Salvar</Text>
+            </TouchableOpacity>
 
-                  <TouchableOpacity
-                  onPress={closeIdeiaModal}
-                  style={{...styles.botaoSalveCancel, marginVertical: 0, position:'absolute', right: 30, backgroundColor: "red"}}
-                  >
-                    <Text style={styles.textBotaoSalveCancel}>Cancelar</Text>
-                  </TouchableOpacity>              
-                </View>
-                <StatusBar style="light"/>
-              </ImageBackground>
-            </Modal> 
+            <TouchableOpacity
+              onPress={closeIdeiaModal}
+              style={{
+                ...styles.botaoSalveCancel,
+                marginVertical: 0,
+                position: "absolute",
+                right: 30,
+                backgroundColor: "red",
+              }}
+            >
+              <Text style={styles.textBotaoSalveCancel}>Cancelar</Text>
+            </TouchableOpacity>
           </View>
-    );
+          <StatusBar style="light" />
+        </ImageBackground>
+      </Modal>
+    </View>
+  );
 };
-  
+
 export default AddIdeias;
--- src/views/Ideias.js
+++ src/views/Ideias.js
@@ -1,105 +1,102 @@
-import React, {useState, useEffect} from "react";
-import {Text, View, TouchableOpacity, ImageBackground, 
-  ScrollView} from 'react-native'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Ionicons } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  ScrollView,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 import { Header } from "react-native-elements";
 import { styles } from "../style/style";
-import AddIdeias from '../components/Ideias/AddIdeias'
-import EditIdeias from '../components/Ideias/EditIdeias'
-import DeletIdeias from '../components/Ideias/DeletIdeias'
+import AddIdeias from "../components/Ideias/AddIdeias";
+import EditIdeias from "../components/Ideias/EditIdeias";
+import DeletIdeias from "../components/Ideias/DeletIdeias";
 
-
-function ListaIdeia ({navigation}) {
-
-  const [isAddIdeiaModalOpen, setIsAddIdeiaModalOpen] = useState(false)
-  const [isEditIdeiaModalOpen, setIsEditIdeiaModalOpen] = useState(false)
-  const [isDeletIdeiaModalOpen, setIsDeletIdeiaModalOpen] = useState(false)
-  const [ideias, setIdeias] = useState([])
-  const [selectedIdeia, setSelectedIdeia] = useState(false)
-
+function ListaIdeia({ navigation }) {
+  const [isAddIdeiaModalOpen, setIsAddIdeiaModalOpen] = useState(false);
+  const [isEditIdeiaModalOpen, setIsEditIdeiaModalOpen] = useState(false);
+  const [isDeletIdeiaModalOpen, setIsDeletIdeiaModalOpen] = useState(false);
+  const [ideias, setIdeias] = useState([]);
+  const [selectedIdeia, setSelectedIdeia] = useState(false);
+  console.log(ideias);
   const toggleAddIdeia = () => {
-    setIsAddIdeiaModalOpen(!isAddIdeiaModalOpen)
-  }
+    setIsAddIdeiaModalOpen(!isAddIdeiaModalOpen);
+  };
 
   const toggleEditIdeia = () => {
-    setIsEditIdeiaModalOpen(!isEditIdeiaModalOpen)
-  }
+    setIsEditIdeiaModalOpen(!isEditIdeiaModalOpen);
+  };
 
   const toggleDeletIdeia = () => {
-    setIsDeletIdeiaModalOpen(!isDeletIdeiaModalOpen)
-  }
-
-  const addIdeia = (data) => {
-    setIdeias([data, ...ideias])    
-  }
+    setIsDeletIdeiaModalOpen(!isDeletIdeiaModalOpen);
+  };
 
   const adicionarIdea = (data) => {
-    try{
-      let idNovaIdeia = ideias[ideias.length - 1 ].id ++
-      data={id:idNovaIdeia, ...data}
-      setIdeias([data, ...setIdeias])
-      console.log(ideias.id)             
-    }catch{
-      data={id:1, ...data}
-      setIdeias([data, ...setIdeias])
-      console.log(ideias.id)
+    try {
+      let idNovaIdeia = ideias[ideias.length - 1].id + 1;
+      data = { id: idNovaIdeia, ...data };
+      setIdeias([...ideias, data]);
+    } catch {
+      data = { id: 1, ...data };
+      setIdeias([...ideias, data]);
     }
-  }
+  };
 
   const editIdeia = (data) => {
-    setIdeias(ideias.map(idea => idea.id == data.id ? data : idea))
-  }
+    setIdeias(ideias.map((idea) => (idea.id == data.id ? data : idea)));
+  };
 
-  const deletIdeia = id => {
-    setIdeias(ideias.filter(idea => idea.id !== id))
-  }
+  const deletIdeia = (id) => {
+    setIdeias(ideias.filter((idea) => idea.id !== id));
+  };
 
-  //vai retornar as ideas que já estão salvas 
-useEffect(() => {
-  async function carregaIdeias(){
-    const ideiaStorage = await AsyncStorage.getItem('@idea');
+  //vai retornar as ideas que já estão salvas
+  useEffect(() => {
+    async function carregaIdeias() {
+      const ideiaStorage = await AsyncStorage.getItem("@idea");
 
-    if(ideiaStorage){
-      setIdeias(JSON.parse(ideiaStorage));
+      if (ideiaStorage) {
+        setIdeias(JSON.parse(ideiaStorage));
+      }
     }
-  }
 
-  carregaIdeias();
+    carregaIdeias();
+  }, []);
 
-}, [])
+  //vai monitorar e atualizar a função carregaIdeias a cada ação, seja de excluir, editar ou add
+  useEffect(() => {
+    async function salveIdeia() {
+      await AsyncStorage.setItem("@idea", JSON.stringify(ideias));
+    }
 
-//vai monitorar e atualizar a função carregaIdeias a cada ação, seja de excluir, editar ou add 
-useEffect(() =>{
-  async function salveIdeia(){
-    await AsyncStorage.setItem('@idea', JSON.stringify(ideias))
-  }
+    salveIdeia();
+  }, [ideias]);
 
-  salveIdeia();
-}, [ideias])
-
-
-  return(
+  return (
     <>
-    <Header
-      containerStyle={{ height: 80, backgroundColor: "#1D1D1D" }}
-      leftComponent={{
-        icon: "menu",
-        color: "#E37B09",
-        onPress: navigation.openDrawer,
-        size: 40,
-      }}
-      centerComponent={{
-        text: "Suas ideias",
-        style: styles.headerText,
-      }}
-    />
-    <ScrollView>
-        <ImageBackground source={require('../images/fundo1.png')} style={styles.bgImage}>
+      <Header
+        containerStyle={{ height: 80, backgroundColor: "#1D1D1D" }}
+        leftComponent={{
+          icon: "menu",
+          color: "#E37B09",
+          onPress: navigation.openDrawer,
+          size: 40,
+        }}
+        centerComponent={{
+          text: "Suas ideias",
+          style: styles.headerText,
+        }}
+      />
+      <ScrollView>
+        <ImageBackground
+          source={require("../images/fundo1.png")}
+          style={styles.bgImage}
+        >
           <View style={styles.container}>
-
             <TouchableOpacity
               onPress={toggleAddIdeia}
               style={styles.botaoaddIdeia}
@@ -107,61 +104,67 @@ useEffect(() =>{
               <Ionicons name="ios-add" size={50} color="#E37B09" />
             </TouchableOpacity>
 
-            {ideias.map((data, index) => 
+            {ideias.map((data, index) => (
               <View style={styles.lista}>
                 <Text style={styles.tituloIdeia}>{data.titulo}</Text>
                 <Text style={styles.descIdeia}>{data.desc}</Text>
-              
-              <View style={styles.acoesLista}>
-                <TouchableOpacity
-                  onPress={() =>{
-                    toggleEditIdeia();
-                    setSelectedIdeia(data)
-                  }}
-                >
-                  <MaterialIcons name="edit" size={32} color="#E37B09" />
-                </TouchableOpacity>
 
-                <TouchableOpacity
-                  onPress={() => {
-                    toggleDeletIdeia();
-                    setSelectedIdeia(data)
-                  }}
-                >
-                  <MaterialIcons name="delete-forever" size={32} color="red" />
-                </TouchableOpacity>
+                <View style={styles.acoesLista}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      toggleEditIdeia();
+                      setSelectedIdeia(data);
+                    }}
+                  >
+                    <MaterialIcons name="edit" size={32} color="#E37B09" />
+                  </TouchableOpacity>
 
+                  <TouchableOpacity
+                    onPress={() => {
+                      toggleDeletIdeia();
+                      setSelectedIdeia(data);
+                    }}
+                  >
+                    <MaterialIcons
+                      name="delete-forever"
+                      size={32}
+                      color="red"
+                    />
+                  </TouchableOpacity>
+                </View>
               </View>
+            ))}
 
-              </View>  
-
-            )}
-
-              {isAddIdeiaModalOpen ? <AddIdeias 
+            {isAddIdeiaModalOpen ? (
+              <AddIdeias
                 openIdeiaModal={isAddIdeiaModalOpen}
                 closeIdeiaModal={toggleAddIdeia}
-                addIdeias={addIdeia}
-                adicionarIdea={adicionarIdea}
-              /> : null}
+                adicionarIdeia={adicionarIdea}
+              />
+            ) : null}
 
-              {isEditIdeiaModalOpen ? <EditIdeias
+            {isEditIdeiaModalOpen ? (
+              <EditIdeias
                 openIdeiaModal={isEditIdeiaModalOpen}
                 closeIdeiaModal={toggleEditIdeia}
                 selectedIdeia={selectedIdeia}
                 editIdeia={editIdeia}
-              />: null}
+              />
+            ) : null}
 
-              {isDeletIdeiaModalOpen ? <DeletIdeias
+            {isDeletIdeiaModalOpen ? (
+              <DeletIdeias
                 openIdeiaModal={isDeletIdeiaModalOpen}
                 closeIdeiaModal={toggleDeletIdeia}
                 selectedIdeia={selectedIdeia}
                 deletIdeia={deletIdeia}
-              />: null}
+              />
+            ) : null}
           </View>
-          <StatusBar style="light"/>
+          <StatusBar style="light" />
         </ImageBackground>
-      </ScrollView>  
+      </ScrollView>
     </>
-  )
+  );
 }
-export default ListaIdeia
+export default ListaIdeia;
