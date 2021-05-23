90b3c0c9ac79fe03cfcfbe6288f0ebe4e2e6522a
Philipe Santos <philipe_02@hotmail.com>
2021-05-22 21:26:42 -0300

    Preparando merge develop

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,26 +4,26 @@ export const EDITA_USUARIO = "EDITA_USUARIO";
 export const EXCLUI_USUARIO = "EXCLUI_USUARIO";
 
 export const logar = (usuarioLogin) => (dispatch) => {
-  dispatch({
-    type: USUARIO_LOGADO,
-    payload: usuarioLogin,
-  });
+    dispatch({
+        type: USUARIO_LOGADO,
+        payload: usuarioLogin,
+    });
 };
 export const cadastrar = (usuarioNovo) => (dispatch) => {
-  dispatch({
-    type: ADD_USUARIO,
-    payload: usuarioNovo,
-  });
+    dispatch({
+        type: ADD_USUARIO,
+        payload: usuarioNovo,
+    });
 };
 export const editar = (usuarioEditado) => (dispatch) => {
-  dispatch({
-    type: EDITA_USUARIO,
-    payload: usuarioEditado,
-  });
+    dispatch({
+        type: EDITA_USUARIO,
+        payload: usuarioEditado,
+    });
 };
 export const excluir = (usuarioDeletar) => (dispatch) => {
-  dispatch({
-    type: EXCLUI_USUARIO,
-    payload: usuarioDeletar,
-  });
+    dispatch({
+        type: EXCLUI_USUARIO,
+        payload: usuarioDeletar,
+    });
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,13 +6,13 @@ import { persistStore, persistReducer } from "redux-persist";
 import UsuarioReducer from "./reducer";
 
 const persistConfig = {
-  key: "root",
-  storage: AsyncStorage,
-  whitelist: ["usuarios"],
+    key: "root",
+    storage: AsyncStorage,
+    whitelist: ["usuarios"],
 };
 
 const rootReducer = combineReducers({
-  UsuarioReducer: persistReducer(persistConfig, UsuarioReducer),
+    UsuarioReducer: persistReducer(persistConfig, UsuarioReducer),
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
