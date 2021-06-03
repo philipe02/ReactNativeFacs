b8a9566c2adf8ba5bd43a377fc1a8ac93d7b2697
nathalieEvans <nathalie.evangelista@hotmail.com>
2021-06-03 09:05:26 -0300

    Links da API no mesmo http-commom

--- http-common.js
+++ http-common.js
@@ -1,8 +1,28 @@
 import axios from 'axios'
 
-export default axios.create({
+const ideas = () => axios.create({
     baseURL: "https://60b14c0962ab150017ae0c67.mockapi.io/",
     headers: {
         "Content-type": "appication/json"
     }
 })
+
+const user = () => axios.create({
+    baseURL: "",
+    headers: {
+        "Content-type": "appication/json"
+    }
+})
+
+const methodology = () => axios.create({
+    baseURL: "",
+    headers: {
+        "Content-type": "appication/json"
+    }
+})
+
+export default {
+    ideas, 
+    user,
+    methodology
+}
