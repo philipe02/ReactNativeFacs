5031a775d2b33bd70c413f0ed61262d99edc613b
DrezaVitorio <deza_vitorio@hotmail.com>
2021-06-11 21:03:06 -0300

    ultimos ajustes

--- http-common.js
+++ http-common.js
@@ -1,8 +1,33 @@
 import axios from 'axios'
 
+/*
 export default axios.create({
     baseURL: "https://60bea5306035840017c1778f.mockapi.io/",
     headers: {
         "Content-type": "application/json"
     }
 })
+*/
+
+const ideas = () => axios.create({
+    baseURL: "https://60b14c0962ab150017ae0c67.mockapi.io/",
+    headers: {
+        "Content-type": "appication/json"
+    }
+})
+
+const user = () => axios.create({
+    baseURL: "",
+    headers: {
+        "Content-type": "appication/json"
+    }
+})
+
+const feed = () => axios.create({
+    baseURL: "https://60bea5306035840017c1778f.mockapi.io/",
+    headers: {
+        "Content-type": "application/json"
+    }
+})
+
+export default {ideas, user, feed}
--- services/ComentarioService.js
+++ services/ComentarioService.js
@@ -1,4 +1,4 @@
-import http from '../http-common';
+import http from '../http-feed';
 
 const getAll = () => {
     return http.get("/comentario")
